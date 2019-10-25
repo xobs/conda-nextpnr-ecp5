7e0fb539a1010b0e8da1048889e2896e10f4a867
Sean Cross <sean@xobs.io>
2019-10-25 14:07:10 +0800

    remove unused code
    Signed-off-by: Sean Cross <sean@xobs.io>

--- bld.ps1
+++ bld.ps1
@@ -24,27 +24,8 @@ Function DeGZip-File{
     Set-PSDebug -Trace 1
 }
 
-# . C:\Users\smc\Miniconda3\shell\condabin\conda-hook.ps1
-
 # Allow us to access System.IO.Compression.GzipStream
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-
-$stage = "$($Env:PREFIX)"
-
-# $vcpkg_path = "D:\code\vcpkg"
-# # $vcpkg_path = "C:\tools\vcpkg"
-# # Set up vcpkg and ensure modules are installed
-# Set-Location $vcpkg_path
-# git rev-parse HEAD
-# if ($LastExitCode -ne 0) { exit $LastExitCode }
-# git checkout $env:VCPKG_COMMIT
-# if ($LastExitCode -ne 0) { exit $LastExitCode }
-# .\bootstrap-vcpkg.bat
-# if ($LastExitCode -ne 0) { exit $LastExitCode }
-# vcpkg integrate install
-# if ($LastExitCode -ne 0) { exit $LastExitCode }
-# vcpkg install boost-filesystem:x64-windows-static boost-program-options:x64-windows-static boost-thread:x64-windows-static boost-python:x64-windows-static eigen3:x64-windows-static boost-dll:x64-windows-static
-# if ($LastExitCode -ne 0) { exit $LastExitCode }
 
 # Validate conda Python environment
 python -V
@@ -61,28 +42,18 @@ DeGZip-File "$($env:RECIPE_DIR)\chipdb\chipdb-45k.bba.gz" "$($env:SRC_DIR)\chipd
 DeGZip-File "$($env:RECIPE_DIR)\chipdb\chipdb-85k.bba.gz" "$($env:SRC_DIR)\chipdb-extract\chipdb-85k.bba"
 $chipdb = "$($env:SRC_DIR.replace("\", "/"))/chipdb-extract"
 
+# cmake requires we use forward slashes, so patch up various environment variables
+# to use forward slashes.
+$prjtrellis = "$($env:LIBRARY_PREFIX.replace("\", "/"))/share/trellis"
+$libtrellis = "$($env:LIBRARY_PREFIX.replace("\", "/"))/share/trellis/libtrellis/release"
+
 Write-Output ""
 Set-Location $env:SRC_DIR
 
-$compiler = "Visual Studio 16 2019"
-$arch = ""
-If ($true) {
-    $compiler = "Visual Studio 15 2017"
-    $arch = ""
-} else {
-    $compiler = "Visual Studio 16 2019"
-    $arch = "-A x64"
-}
-
-# cmake "-DCMAKE_TOOLCHAIN_FILE=$($vcpkg_path.replace("\", "/"))/scripts/buildsystems/vcpkg.cmake" -DARCH=ecp5 "-DTRELLIS_ROOT=$prjtrellis" "-DPYTRELLIS_LIBDIR=$libtrellis" "-DPREGENERATED_BBA_PATH=$chipdb" "-DCMAKE_INSTALL_PREFIX=$stage" -DVCPKG_TARGET_TRIPLET=x64-windows-static -G "Visual Studio 16 2019" -A "x64" -DSTATIC_BUILD=ON -DBUILD_GUI=OFF .
-
-
-# cmake requires we use forward slashes, so patch up various environment variables
-# to use forward slashes.
-Get-ChildItem $Env:LIBRARY_PREFIX
-$prjtrellis = "$($env:LIBRARY_PREFIX.replace("\", "/"))/share/trellis"
-$libtrellis = "$($env:LIBRARY_PREFIX.replace("\", "/"))/share/trellis/libtrellis/release"
-cmake "-DCMAKE_TOOLCHAIN_FILE=$($vcpkg_path.replace("\", "/"))/scripts/buildsystems/vcpkg.cmake" -DARCH=ecp5 "-DTRELLIS_ROOT=$prjtrellis" "-DPYTRELLIS_LIBDIR=$libtrellis" "-DPREGENERATED_BBA_PATH=$chipdb" "-DCMAKE_INSTALL_PREFIX=$stage" -DVCPKG_TARGET_TRIPLET=x64-windows-static -G "Visual Studio 15 2017" -A "x64" -DSTATIC_BUILD=ON -DBUILD_GUI=OFF .
+# Configure cmake
+cmake -DARCH=ecp5 "-DTRELLIS_ROOT=$prjtrellis" "-DPYTRELLIS_LIBDIR=$libtrellis" "-DPREGENERATED_BBA_PATH=$chipdb" "-DCMAKE_INSTALL_PREFIX=$env:PREFIX" -G $env:CMAKE_GENERATOR -DSTATIC_BUILD=ON -DBUILD_GUI=OFF .
 if ($LastExitCode -ne 0) { exit $LastExitCode }
-cmake --build . --target install --config Release
+
+# Perform the build
+cmake --build . --target install --config Release --verbose
 if ($LastExitCode -ne 0) { exit $LastExitCode }
